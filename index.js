4d7c5e329650d17785a099aa40ce41701fe58390
vyom Priyesh <vyompriyesh@gmail.com>
2025-07-05 15:50:44 +0530

    add

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ import index from './routers/index.js';
 import auth from './routers/auth.js';
 import categories from './routers/categories.js';
 import dotenv from 'dotenv';
+import cors from 'cors'
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors({
+  origin: '*', // Allow requests from this origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow these HTTP methods
+  allowedHeaders: ['Content-Type'], // Allow these headers
+}));
 
 // Middleware
 app.use(express.json());

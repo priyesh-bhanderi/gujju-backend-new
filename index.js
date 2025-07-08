387a9541836adacae3717f1c68f19dab5b6784f4
vyom Priyesh <vyompriyesh@gmail.com>
2025-07-08 12:11:15 +0530

    master

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "*" }));
 
 app.use('/', index);
 app.use('/api/auth', auth);

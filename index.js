aadb1217b0797c704b64eec885007aecff3951a4
vyom Priyesh <vyompriyesh@gmail.com>
2025-07-08 12:15:11 +0530

    master

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ import projects from './routers/projects.js';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import apis from './routers/apis.js';
-import path from 'path'
-import { fileURLToPath } from 'url';
+// import path from 'path'
+// import { fileURLToPath } from 'url';
 
 dotenv.config();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
